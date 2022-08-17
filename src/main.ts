30027499e27a8e9028e480d068db6066f54971ad
ankit01-oss <ankit.anand@signoz.io>
2022-08-17 20:54:58 +0530

    Add existing files to Git

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const tracer = require('./tracer')
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3002);
+}
+bootstrap();
